8c88bd73ede9908d439440e93c68f61c62ff71ab
Huda Shakir <huda.f.shakir@gmail.com>
2026-02-18 10:53:00 +0300

    feat: add KnowledgeBase module with initial structure.

---
+++ Modules/KnowledgeBase/database/seeders/KnowledgeBaseDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\KnowledgeBase\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class KnowledgeBaseDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/KnowledgeBase/resources/views/index.blade.php
@@ -0,0 +1,5 @@
+<x-knowledgebase::layouts.master>
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('knowledgebase.name') !!}</p>
+</x-knowledgebase::layouts.master>
