93ab4e11fc2801d1e8ff7303ec8783b5b468d110
Mahran_AD <mhranabwdqt971@gmail.com>
2026-03-17 14:22:07 +0300

    test pipline 3.4

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -11,4 +11,4 @@ Route::prefix('v1')->group(function () {
     Route::middleware(['auth:api'])->group(function () {
         Route::post('logout', [SessionController::class, 'destroy'])->name('logout');
     });
-});
+ });
