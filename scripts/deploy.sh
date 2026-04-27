9b876ca9b2c346efce738e1268674cea0d9f9c9f
Mahran_AD <mhranabwdqt971@gmail.com>
2026-04-27 19:42:58 +0300

    Update deploy script to use environment variable for APP_PATH

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-APP_PATH="/home/hudashakir/domains/workflow-api.hudashakir.serv00.net/public_html"   # يُحدَّد من المتغيرات أو اكتبه مباشرة
+APP_PATH="${1:-$SERV00_PATH_PRODUCTION}"   # يُحدَّد من المتغيرات أو اكتبه مباشرة
 
 if [ -z "$APP_PATH" ]; then
     echo "Error: APP_PATH is not set. Please provide it as an argument or set SERV00_PATH."
