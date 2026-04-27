33090d0e72d824bce2374e9aa45699d0dff4c0b8
Mahran_AD <mhranabwdqt971@gmail.com>
2026-04-27 20:37:11 +0300

    Added conditional check for APP_PATH and improved code formatting

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,14 @@ set -e
 APP_PATH="$SERV00_PATH_PRODUCTION"   # يُحدَّد من المتغيرات أو اكتبه مباشرة
 # مثال: APP_PATH="/home/USERNAME/domains/yourdomain.com/public_html"
 
-cd $APP_PATH
+# APP_PATH="/home/hudashakir/domains/workflow-api.hudashakir.serv00.net/public_html"   # يُحدَّد من المتغيرات أو اكتبه مباشرة
+
+if [ -z "$APP_PATH" ]; then
+    echo "Error: APP_PATH is not set. Please provide it as an argument or set SERV00_PATH."
+    exit 1
+fi
+
+cd "$APP_PATH"
 
 echo "📦 Installing dependencies..."
 /usr/local/bin/composer install --no-dev --optimize-autoloader --no-interaction
