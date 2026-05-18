4344bbbc20232633014f7714328e2589ac897f2f
Huda Shakir <--global>
2026-05-18 17:01:25 +0300

    Add roles to LoginSuccessResource response

--- Modules/Auth/app/Http/Resources/LoginSuccessResource.php
+++ Modules/Auth/app/Http/Resources/LoginSuccessResource.php
@@ -32,6 +32,7 @@ class LoginSuccessResource extends JsonResource
                     'id' => $this->tenant->id,
                     'business_name' => $this->tenant->business_name,
                 ]),
+                'roles' => $this->whenLoaded('roles', fn () => $this->roles->pluck('name')),
             ],
         ];
     }
