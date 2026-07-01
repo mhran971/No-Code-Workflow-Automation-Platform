4fc7551dd5e687250bb0d510fc2d197fb5a708f4
Huda Shakir <--global>
2026-07-01 15:48:40 +0300

    Add Pusher broadcasting configuration and update composer dependencies

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -6,6 +6,17 @@ return [
 
     'connections' => [
 
+        'pusher' => [
+            'driver' => 'pusher',
+            'key'    => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
+                'useTLS'  => true,
+            ],
+        ],
+
         'reverb' => [
             'driver'  => 'reverb',
             'key'     => env('REVERB_APP_KEY'),
