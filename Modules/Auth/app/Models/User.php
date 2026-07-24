27a347147a07e82a00a43c1ff0ca5604547ba2e9
Mahran_AD <mhranabwdqt971@gmail.com>
2026-07-24 13:17:26 +0300

    feat(auth): add Notifiable trait and FCM routing to User model

--- Modules/Auth/app/Models/User.php
+++ Modules/Auth/app/Models/User.php
@@ -3,13 +3,24 @@
 namespace Modules\Auth\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Modules\Auth\Enums\Role;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens;
+    use HasApiTokens, Notifiable;
+
+    public function deviceTokens()
+    {
+        return $this->hasMany(DeviceToken::class);
+    }
+
+    public function routeNotificationForFcm(): array
+    {
+        return $this->deviceTokens()->pluck('token')->all();
+    }
 
     /**
      * The attributes that are mass assignable.
