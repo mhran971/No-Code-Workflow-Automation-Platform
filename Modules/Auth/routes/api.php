85dce17fc04d798b11d2675c4e249824f55656a5
Mahran_AD <mhranabwdqt971@gmail.com>
2026-07-24 13:17:35 +0300

    feat(auth): add device tokens management controller and endpoints

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Auth\Http\Controllers\DeviceTokenController;
 use Modules\Auth\Http\Controllers\RegisterController;
 use Modules\Auth\Http\Controllers\SessionController;
 
@@ -11,5 +12,7 @@ Route::prefix('v1')->group(function () {
     Route::middleware(['auth:api', 'active.user'])->group(function () {
         Route::get('me', [SessionController::class, 'me'])->name('me');
         Route::post('logout', [SessionController::class, 'destroy'])->name('logout');
+        Route::post('mobile/device-tokens', [DeviceTokenController::class, 'store'])->name('mobile.device-tokens.store');
+        Route::delete('mobile/device-tokens', [DeviceTokenController::class, 'destroy'])->name('mobile.device-tokens.destroy');
     });
 });
