90bdd9cda047317c7169f4831a2c93a2003a2fc0
Mahran_AD <mhranabwdqt971@gmail.com>
2026-08-18 02:43:18 +0300

    feat(filament): create WorkflowInstanceResource for lightweight execution monitoring

---
+++ app/Filament/Resources/WorkflowInstanceResource/Pages/ListWorkflowInstances.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\WorkflowInstanceResource\Pages;
+
+use App\Filament\Resources\WorkflowInstanceResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListWorkflowInstances extends ListRecords
+{
+    protected static string $resource = WorkflowInstanceResource::class;
+}
